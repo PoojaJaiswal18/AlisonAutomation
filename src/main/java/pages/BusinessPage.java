a8b5361efc5d6de253e618450bc856b911e05887
Jaiswal <2506400@cognizant.com>
2026-07-17 13:43:29 +0530

    add business page

---
+++ src/main/java/pages/BusinessPage.java
@@ -0,0 +1,26 @@
+package pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+import utils.WaitUtils;
+
+public class BusinessPage {
+	
+	private WebDriver driver;
+	private WaitUtils wait;
+	
+	private By requestDemoBtn=By.xpath(null);
+	
+	public BusinessPage(WebDriver driver) {
+		this.driver=driver;
+		this.wait=new WaitUtils(driver,15);
+		
+	}
+	
+	
+	public void clickREquestDemo() {
+		wait.waitForClickability(requestDemoBtn).click();
+	}
+
+}
